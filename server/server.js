07424620630b9a83a0aaa018cf34d6ae5ea1dbc1
David de Sousa <admin@anatta.local>
2017-11-11 16:14:52 -0800

    Utilizes coinpayment npm module

--- server/server.js
+++ server/server.js
@@ -1,25 +1,50 @@
-var express = require("express");
-var app = express();
-var parser = require("body-parser");
-var Database = require("./db");
-var routes = require("./routes");
-var wallet = require("./util/wallet.js");
-var helmet = require("helmet");
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const CoinPayments = require("coinpayments");
+const Database = require("./db");
+const routes = require("./routes");
+const wallet = require("./util/wallet.js");
+const helmet = require("helmet");
+const coinPayments = require("./util/coinPayments.js");
 
 app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
 app.use(express.static("client/build"));
-app.use("/", routes);
-app.use(
-  helmet.contentSecurityPolicy({
-    defaultSrc: ["'self'"],
-    scriptSrc: ["*.google-analytics.com"],
-    styleSrc: ["'unsafe-inline'"],
-    imgSrc: ["*.google-analytics.com"],
-    connectSrc: ["'none'"],
-    fontSrc: [],
-    objectSrc: [],
-    mediaSrc: [],
-    frameSrc: []
-  })
-);
+
+let events = CoinPayments.events;
+
+let middleware = [
+  CoinPayments.ipn({
+    merchantId: process.env.COINPAYMENTS_MERCHANT_ID,
+    merchantSecret: process.env.COINPAYMENTS_MERCHANT_SECRET
+  }),
+  (req, res, next) => {
+    // Handle via middleware
+    console.log(req.body);
+  }
+];
+
+app.use("/", middleware);
+
+// Handle via instance
+events.on("ipn_fail", function(data) {
+  // Handle failed transaction
+  console.log("IPN FAIL");
+  console.log(data);
+});
+events.on("ipn_pending", function(data) {
+  // Handle pending payment
+  console.log("IPN PENDING");
+  console.log(data);
+});
+events.on("ipn_complete", function(data) {
+  // Handle completed payment
+  console.log("IPN COMPLETE");
+  console.log(data);
+});
+
 app.listen(3000, console.log("Listening on 3000"));
+
+console.log(coinPayments.createTransaction(100, "hey@gmail.com"));
---
+++ server/util/coinPayments.js
@@ -0,0 +1,21 @@
+var Coinpayments = require('coinpayments');
+var client = new Coinpayments({
+    key: process.env.COIN_PUB,
+    secret: process.env.COIN_PRIV
+}); 
+
+createTransaction = (amount, buyerEmail) => {
+    const options = {
+        amount,
+        currency1: "USD",
+        currency2: "ETH",
+        buyer_email: buyerEmail  
+    }
+    client.createTransaction(options, (err, res) => {
+        console.log(res);
+    })
+}
+
+module.exports = {
+    createTransaction
+};
